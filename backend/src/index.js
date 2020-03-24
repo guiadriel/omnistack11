498391feaf731544870c34f0be6d4239d8d6d25d
Guilherme Adriel da Rocha <gui.adriel@gmail.com>
2020-03-24 18:51:57 -0300

    back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = requre('cors');
+const routes  = require('./routes');
+
+const app = express();
+
+// É necessário informar qual é o tipo de requisições
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
